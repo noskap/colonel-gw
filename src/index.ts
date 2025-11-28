ff02d779ba8d0246ece8bf516b24c49fc7cc7146
meat <noskap@outlook.com>
2025-11-28 20:18:49 +1030

    Reworked logic to support webhook push instead of discord api

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,14 @@
 import * as dotenv from 'dotenv';
-import { Client, GatewayIntentBits } from 'discord.js';
 import { Bot } from './bot';
 
 // Load environment variables
+
+console.log('initializing config')
 dotenv.config();
-
-// Create a new Discord client
-const client = new Client({
-  intents: [
-    GatewayIntentBits.Guilds,
-    GatewayIntentBits.GuildMessages
-  ]
-});
-
+console.log('Config init done', !!process.env)
 // Initialize the bot
 const bot = new Bot();
 
-// Login to Discord with your token
-client.login(process.env.BOT_TOKEN);
-
-// When client is ready
-client.once('ready', () => {
-  console.log(`Logged in as ${client.user?.tag}!`);
-  bot.startSendingMessages(client);
-});
+// Start sending messages
+console.log('Starting webhook messenger...');
+bot.startSendingMessages();
