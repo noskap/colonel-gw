fc5690013701bda41821ccbb9f8742af4a86393b
l is real <apnosk@gmail.com>
2026-02-01 22:16:22 +1030

    Remove unused env init

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,5 @@
 import { Bot } from './bot';
 
-// Load environment variables
-
-console.log('initializing config')
-console.log('Config init done', !!process.env)
 // Initialize the bot
 const bot = new Bot();
 
